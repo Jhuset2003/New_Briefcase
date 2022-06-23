cb35ba6ec1f37f4941a13de9bd293ce2de34a09a
jhuset2003 <jhusetqp@gmail.com>
2022-06-23 12:02:30 -0500

    Cell version 1.3

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import MarginLayout from "../layout/MarginLayout"
-
+import HomeStyles from "./PagesStyles/Home.module.css"
+import log from "../Img/Log.png"
 
 function Home() {
   return (
    <>
    <MarginLayout>
-      <div>
+    <div className={HomeStyles.Container}>
+      <div className={HomeStyles.LettersContainer}>
         <h1>Portafolio Virtual</h1>
         <h2>Albert Quintanilla </h2>
-        
       </div>
+      <div className={HomeStyles.LogContainer}>
+        <img src={log} alt="" />
+      </div>
+      <div>
+        <p className={HomeStyles.Text}>
+          Desallorrador <strong> FrontEnd</strong> ambicioso y siempre dispuesto a aprender
+        </p>
+      </div>
+    </div>
    </MarginLayout>
    </>
   )
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,14 +5,14 @@ import NavStyle from "./Nav.module.css"
 function Nav() {
   return (
     <div className={NavStyle.WaveContainer}>
-      <Wave mask="url(#mask)" fill="red" options={{ speed:0.25, amplitude:20}}>
+      <Wave mask="url(#mask)" fill="white" options={{ speed:0.25, amplitude:20}}>
         <defs>
           <linearGradient id="gradient" gradientTransform="rotate(90)">
             <stop offset="0.23" stopColor="white" />
             <stop offset="0.5" stopColor="black" />
           </linearGradient>
           <mask id="mask">
-            <rect x="0" y="0" width="2000" height="103" fill="url(#gradient)"  />
+            <rect x="0" y="0" width="2000" height="104" fill="url(#gradient)"  />
           </mask>
         </defs>
     </Wave>
